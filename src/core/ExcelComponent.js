736060ad1cd0a4e70abb0fd657148fe0406c6a25
Kurban <Mr.gustav009@gmail.com>
2020-07-15 14:23:35 +0300

    core

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,9 @@
+import DomListener from '@core/DomListener'
+
+export default class ExcelComponent extends DomListener {
+  static className = ''
+  // Возвращает шаблон компонента
+  toHTML() {
+    return ''
+  }
+}
---
+++ src/core/dom.js
@@ -0,0 +1,37 @@
+class Dom {
+  constructor(selector) {
+    this.$el = typeof selector === 'string'
+      ? document.querySelector(selector)
+      : selector
+  }
+
+  html(str){
+    if (typeof str === 'string'){
+      this.$el.innerHTML = str;
+      return this;
+    }
+    return this.$el.outerHTML.trim()
+  }
+
+  append(node){
+    if(node instanceof Dom){
+      node = node.$el;
+    }
+    this.$el.append(node);
+
+    return this;
+  }
+}
+
+export function $(el){
+  return new Dom(el);
+}
+
+$.create = (elName, classes) => {
+  let $node = document.createElement(elName);
+  
+  if(classes){
+    $node.className = classes;
+  }
+  return $($node);
+}
