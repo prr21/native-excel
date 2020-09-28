c0d5d0ca2a85700dfe91064d5ab965329263e6b4
Kurban <Mr.gustav009@gmail.com>
2020-09-28 23:59:09 +0300

    Observer

---
+++ src/core/Observer.js
@@ -0,0 +1,23 @@
+export class Observer {
+  constructor() {
+    this.listeners = {}
+  }
+
+  dispatch(event, ...args) {
+    if (!Array.isArray(this.listeners[event])) {
+      return false
+    }
+    this.listeners[event].forEach(fn => fn(args))
+    return true
+  }
+
+  subscribe(event, fn) {
+    this.listeners[event] = this.listeners[event] || []
+    this.listeners[event].push(fn)
+
+    return () => {
+      this.listeners[event] =
+        this.listeners[event].filter(listener => listener !== fn)
+    }
+  }
+}
